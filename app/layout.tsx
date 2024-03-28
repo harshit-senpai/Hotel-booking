bace9ec79317274b9a14b382b7236e8ca244a5c9
harshit Mishra <miananya2@gmail.com>
2024-03-29 02:26:15 +0530

    model UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Modal isOpen />
+        <Modal actionLabel="Submit" isOpen />
         <Navbar />
         {children}
       </body>
