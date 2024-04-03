b448157017b3c854f87570c709511a8e6dfbeaeb
harshit Mishra <miananya2@gmail.com>
2024-04-03 16:08:28 +0530

    making query string

--- components/CategoryBox.tsx
+++ components/CategoryBox.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
   icon: IconType;
@@ -13,8 +15,38 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = () => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updateQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updateQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updateQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
         selected ? "border-b-neutral-800" : "border-transparent"
       } ${selected ? "text-neutral-800" : "text-neutral-500"}`}
