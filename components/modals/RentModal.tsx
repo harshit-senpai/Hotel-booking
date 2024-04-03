098213245978542458a49d5f9249e9f15b73ebae
harshit Mishra <miananya2@gmail.com>
2024-04-03 23:46:50 +0530

    creating steps of Listing

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -2,8 +2,44 @@
 
 import useRentModal from "@/hooks/useRentModal";
 import Modal from "./Modal";
+import { useState } from "react";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = () => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  };
+
+  const secondaryActionLabel = () => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  };
+
   const rentModal = useRentModal();
   return (
     <Modal
