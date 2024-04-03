a08a1559e38eb7558a9f90f0622d4b2b26179b40
harshit Mishra <miananya2@gmail.com>
2024-04-03 16:28:07 +0530

    more categories

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -1,9 +1,21 @@
-import { TbBeach } from "react-icons/tb";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import Container from "../Container";
-import { GiWindmill } from "react-icons/gi";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
+import { IoDiamond } from "react-icons/io5";
 
 export const Category = [
   {
@@ -20,6 +32,66 @@ export const Category = [
     label: "Modern",
     icon: MdOutlineVilla,
     description: "This property is modern.",
+  },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is on countryside.",
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has pool.",
+  },
+  {
+    label: "Island",
+    icon: GiIsland,
+    description: "This property on Island.",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is Near a lake.",
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property has Skiing activities.",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is in a castle.",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property has camping activities.",
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is in Arctic.",
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description: "This property is in cave.",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in desert.",
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is in barn!.",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is Luxurious!.",
   },
 ];
 
