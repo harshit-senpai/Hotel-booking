896e7d273853e8d451add1dd683d2939cf75539d
harshit Mishra <miananya2@gmail.com>
2024-04-11 04:03:49 +0530

    home-page listing-card heart-button

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
+import ListingCard from "@/components/listings/ListingCard";
+import getCurrentUser from "@/lib/actions/getCurrentUser.action";
+import getListings from "@/lib/actions/getListings.action";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return <EmptyState showReset />;
   }
   return (
     <Container>
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cold-3 lg:grid-cold-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        My future listings
+      <div className="pt-32 grid grid-cols-1 sm:grid-cols-2 md:grid-cold-3 lg:grid-cold-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
       </div>
     </Container>
   );
---
+++ components/HeartButton.tsx
@@ -0,0 +1,31 @@
+import { SafeUser } from "@/types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface HeartButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  const hasFavorited = false;
+  const toggleFavourite = () => {};
+  return (
+    <div className="relative hover:opacity-80 transition cursor-pointer">
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={26}
+        className={`${
+          hasFavorited ? "fill-rose-500" : "fill-neutral-500/70 "
+        } `}
+      />
+    </div>
+  );
+};
+
+export default HeartButton;
