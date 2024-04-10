7222367d9d37851a59be810043367ae88595e770
harshit Mishra <miananya2@gmail.com>
2024-04-11 02:12:50 +0530

    No result ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return <EmptyState showReset />;
+  }
   return (
-    <div>
-      
-      Hello Next
-    </div>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cold-3 lg:grid-cold-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        My future listings
+      </div>
+    </Container>
   );
 }
