06031b37f8c78843007e739f49e79ab6af6e5762
harshit Mishra <miananya2@gmail.com>
2024-04-14 05:24:48 +0530

    unnesecarry export removal

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/utils/prismadb";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
